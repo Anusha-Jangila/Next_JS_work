eb84b85b4d391c9091142e81220a144123979daa
Anusha-Jangila <anusha.niu@gmail.com>
2025-05-27 17:52:31 -0500

    Add files via upload

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+
+export default function HomePage() {
+  return (
+    <main>
+      <Header />
+      <h1>Welcome to my site!</h1>
+    </main>
+  );
+}
