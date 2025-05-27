eee68f33c763af4b4dfa62e12ecbb40cdb36c3c6
Anusha-Jangila <anusha.niu@gmail.com>
2025-05-27 17:51:58 -0500

    Create page.js

---
+++ src/app/multistep/page.js
@@ -0,0 +1,9 @@
+import MultiStepForm from '@/components/MultiStepForm';
+export default function MultiStepPage(){
+    return (
+        <div className="p-10">
+            <h1 className="text-2xl mb-6">Multi-Step Form</h1>
+            <MultiStepForm />
+        </div>
+    );
+}
